da816c14b467bba599b8dc55ebc73af7e4539e46
magentagecko <137463883+magentagecko@users.noreply.github.com>
2023-06-30 18:27:57 +0700

    Update histats.js

--- histats.js
+++ histats.js
@@ -1,9 +1,9 @@
 var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4774269,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
+_Hasync.push([&#39;Histats.start&#39;, &#39;1,4778719,4,0,0,0,00010000&#39;]);
+_Hasync.push([&#39;Histats.fasi&#39;, &#39;1&#39;]);
+_Hasync.push([&#39;Histats.track_hits&#39;, &#39;&#39;]);
 (function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+var hs = document.createElement(&#39;script&#39;); hs.type = &#39;text/javascript&#39;; hs.async = true;
+hs.src = (&#39;//s10.histats.com/js15_as.js&#39;);
+(document.getElementsByTagName(&#39;head&#39;)[0] || document.getElementsByTagName(&#39;body&#39;)[0]).appendChild(hs);
 })();
